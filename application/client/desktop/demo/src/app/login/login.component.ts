acb980eb6257f50ae09fcea9257d284088a7a3e9
aharishsundhar <aharishsundhar>
2020-11-20 11:46:42 +0000

    updated generated code

--- application/client/desktop/demo/src/app/login/login.component.ts
+++ application/client/desktop/demo/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { LoginService } from './login.service';
+
 
 
 
@@ -11,36 +11,14 @@ import { LoginService } from './login.service';
 })
 export class LoginComponent implements OnInit {
 
-users = {
-   name: '',
-   email: '',
-   age: ''
-};
-
   constructor(
-private loginService: LoginService
+
   ) { }
 
   ngOnInit() {
     
   }
 
-GpCreate() {
- this.loginService.GpCreate(this.users)
-  .subscribe(
-    data => {
-       console.log('data created successfully');
-this.users.name = '';
 
-this.users.email = '';
-
-this.users.age = '';
-
-    },
-    error => {
-       console.log('cannot able to create the data');
-    }
-    );
-}
 
 }
--- application/client/desktop/demo/src/app/login/login.service.ts
+++ application/client/desktop/demo/src/app/login/login.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { SharedService } from '../../shared/shared.service';
+
+
 
 @Injectable({
     providedIn: 'root'
@@ -9,12 +8,9 @@ import { SharedService } from '../../shared/shared.service';
 export class LoginService {
 
   constructor(
-private http: HttpClient,
-private sharedService: SharedService
+
   ) { }
 
-GpCreate(users: any): Observable<any> {
- return this.http.post(this.sharedService.DESKTOP_API + '/users', users);
-}
+
 
 }
